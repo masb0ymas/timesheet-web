0ae4bde73c821b9ff36f16edc400f40e6dbfcec9
masb0ymas <n.fajri@outlook.com>
2024-09-29 11:51:35 +0700

    fix: update env

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
