dd96bf429391bddcc2e6879705516b498d56eee6
masb0ymas <n.fajri@outlook.com>
2024-09-29 11:53:28 +0700

    fix: improve ui/button

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -4,7 +4,9 @@ import React from "react"
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   fullWidth?: boolean
   styleProps?: string
+  variant?: "default" | "outline"
 }
+
 type ButtonInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   fullWidth?: boolean
   styleProps?: string
@@ -12,13 +14,19 @@ type ButtonInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 }
 
 export function Button(props: ButtonProps) {
+  let variant = "text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600"
+
+  if (props.variant === "outline") {
+    variant = "border text-indigo-600 border-indigo-600 hover:text-white hover:bg-indigo-500 focus-visible:outline-indigo-600 transition duration-150 ease-in-out"
+  }
+
   return (
     <button
       className={clsx(
         "flex",
         props.fullWidth ? "w-full" : "w-fit",
-        "w-full",
-        "justify-center items-center rounded-lg bg-indigo-600 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+        variant,
+        "justify-center items-center rounded-lg px-3 py-2 text-sm font-semibold leading-6 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ",
         props.styleProps
       )}
       {...props}
