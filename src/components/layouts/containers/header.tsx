e0c44e194b3358ff019db380db289d42e83e29ee
masb0ymas <n.fajri@outlook.com>
2024-10-02 11:50:41 +0700

    fix: logo

--- src/components/layouts/containers/header.tsx
+++ src/components/layouts/containers/header.tsx
@@ -73,7 +73,7 @@ export default function Header() {
                 <div className="flex-shrink-0">
                   <img
                     alt="Your Company"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
                     className="h-8 w-8"
                   />
                 </div>
